7f69fc2c702b8a1401751e4c4aa87e16e80a8565
paulfloyd <paulbitsindou@gmail.com>
2019-04-07 06:17:18 +0200

    transfert de fichier sur git

---
+++ lib/code_site.rb
@@ -0,0 +1,24 @@
+#Demander le nom de l'utilisateur
+def user_name
+  puts "Hey,Comment tu t'appelles??"
+  print user_name
+  user_name = gets.chomp
+
+end
+user_name
+#Dire bonjour à l'utilisateur
+def say_hello(user_name)
+  puts "Bonjour #{user_name}"
+
+end
+say_hello
+#liste de questions
+def question_list
+
+
+end
+question_list
+def ask_question
+  puts ""
+
+end
